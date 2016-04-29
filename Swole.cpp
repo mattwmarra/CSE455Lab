b93147d424fba0ccc00bc663e4cbb917e3ea98cf
CesarAGuerrero <rasecz@hotmail.com>
2016-04-28 19:51:01 -0700

    Swole.cpp
    The main function for a C++ version of The Swole App. Project.

---
+++ Swole.cpp
@@ -0,0 +1,25 @@
+/*****************************************************
+Users: Cesar Guerrero, Matt, Billy
+Project: The Swole App
+Date: April 28, 2016
+Description: This app is meant to keep track of ......... for users' health...... //matt
+*****************************************************/
+
+#include "Swole.h"
+#include <iostream>
+
+using namespace std;
+
+int main() 
+{
+	
+
+
+
+
+
+
+
+}
+
+
