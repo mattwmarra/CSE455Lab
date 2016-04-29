d4d2aed1f6208282ffcf433b6f83a008d0bbd1bf
MATT MARRA <thatmarrakid@gmail.com>
2016-04-28 18:14:09 -0700

    Created User Functions
    User class functions, to be converted to Java and Swift functions and
    implemented into the application

---
+++ user funcitons.cpp
@@ -0,0 +1,25 @@
+#include "iostream"
+##include "string"
+
+using namespace std;
+
+class Person{
+public:
+	Person(int name, int height, float weight); //Constructor function
+	int getHeight(); // Get user height
+	string getName(); // Get User Name
+	double getWeight(); //Get user weight
+	int getBMI(); //Get user BMI
+	int adjustBMI(int height, float weight); // adjust user BMI, to be called every time the height or weight of a user changes
+private:
+	string name;
+	int height;
+	double weight;
+	int BMI;
+};
+
+int main(int argc, char const *argv[])
+{
+	
+	return 0;
+}
